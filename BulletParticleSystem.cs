367e2c0ce809015d35c0f4bcbd77b7638057ae81
jiexdrop <jorge.noogueira@gmail.com>
2021-01-03 18:03:58 +0100

    Add BulletParticleSystem

---
+++ BulletParticleSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletParticleSystem : MonoBehaviour
+{
+    public static BulletParticleSystem Instance;
+
+    private ParticleSystem particles;
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    public void Emit(Vector3 position, float angle, float speed)
+    {
+        ParticleSystem.EmitParams emitParams = new ParticleSystem.EmitParams();
+        emitParams.position = position;
+        //emitParams.velocity = velocity;
+        //emitParams.applyShapeToPosition = true;
+        //emitParams.rotation = 45.0f;
+        //emitParams.angularVelocity = angle;
+        //emitParams.angularVelocity3D = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+        emitParams.velocity = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * speed;
+        particles.Emit(emitParams, 1);
+    }
+
+}
